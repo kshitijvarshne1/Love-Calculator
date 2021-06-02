4e05c16efa5782ced60e79a30d6a25bad3b413d1
kshitij varshney <kshitijvarshney92@gmail.com>
2021-06-03 00:02:12 +0530

    final commit

--- script.js
+++ script.js
@@ -1,6 +1,23 @@
 
 function checkLovePercentage(){
     var loveData = Math.floor(Math.random()*100)+1;
-    document.getElementById('loveValue').value=loveData+"%";
+    var name = document.getElementById('name');
+    var lName= document.getElementById('loveName');
+
+    if(name===""){
+        alert("Please enter your name")
+    }
+    else if(lName===""){
+        alert("Please enter your love name")
+    }
+    else if(name.length<3){
+        alert("Name length should greater than 3")
+    }
+    else if(name.length<3){
+        alert("Love Name length should greater than 3")
+    }
+    else{
+        document.getElementById('loveValue').value=loveData+"%";
+    }
 
 }
