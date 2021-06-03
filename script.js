303a0d8b62cdaf0f807975ec6fab06df56eff173
kshitij varshney <kshitijvarshney92@gmail.com>
2021-06-03 18:23:30 +0530

    add validation

--- script.js
+++ script.js
@@ -3,17 +3,17 @@ function checkLovePercentage(){
     var loveData = Math.floor(Math.random()*100)+1;
     var name = document.getElementById('name');
     var lName= document.getElementById('loveName');
-
-    if(name===""){
+    
+    if(name.value==""){
         alert("Please enter your name")
     }
-    else if(lName===""){
+    else if(lName.value==""){
         alert("Please enter your love name")
     }
-    else if(name.length<3){
+    else if(name.value.length<3){
         alert("Name length should greater than 3")
     }
-    else if(name.length<3){
+    else if(lname.value.length<3){
         alert("Love Name length should greater than 3")
     }
     else{
